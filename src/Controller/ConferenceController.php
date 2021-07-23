30e1fc854a5b08952d0ef1eaf0fc0af0138aa60a
Victor Arocha <victor.arocha@inblay.mx>
2021-07-23 00:11:12 -0500

    Testing with Response

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,16 +3,28 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/conference', name: 'conference')]
-    public function index(): Response
+    #[Route('/', name: 'homepage')]
+    public function index(Request $request): Response
     {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);
+        $greet = '';
+        if ($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s</h1>', htmlspecialchars($name));
+        }
+
+        return new Response(<<<EOF
+        <html>
+            <body>
+                $greet
+                <img src="/images/under-construction.gif" />
+            </body>
+        </html>
+        EOF
+        );
     }
 }
